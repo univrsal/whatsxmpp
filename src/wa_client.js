d8834db1d7ff5d5f5463ad9e308b33c67783f1ed
univrsal <uni@vrsal.de>
2021-09-04 12:27:00 +0200

    WhatsApp Client: React to state change
    and notify xmpp if we are disconnected for too long

--- src/wa_client.js
+++ src/wa_client.js
@@ -1,21 +1,22 @@
-const { Client } = require('whatsapp-web.js');
-var log = require('./log');
-var QRCode = require('qrcode');
-var fs = require('fs');
+const { Client } = require("whatsapp-web.js");
+var log = require("./log");
+var QRCode = require("qrcode");
+var fs = require("fs");
 
 class WAClient {
     constructor(bridge) {
         this.session_cfg = null;
         this.bridge = bridge;
+        this.connected = false;
         bridge.whatsapp = this;
-        fs.open('./whats_app_client_session.json', 'r', (err, fd) => {
+        fs.open("./whats_app_client_session.json", "r", (err, fd) => {
             if (err) {
-                if (err.code === 'ENOENT') {
+                if (err.code === "ENOENT") {
                     log.info(
-                        'WhatsApp session doesn\'t exist, creating new one.'
+                        "WhatsApp session doesn't exist, creating new one."
                     );
                 } else {
-                    log.error('Error while reading WhatsApp session: ' + err);
+                    log.error("Error while reading WhatsApp session: " + err);
                 }
             } else {
                 try {
@@ -32,16 +33,41 @@ class WAClient {
             });
             this.client.initialize();
             this.qr_code = undefined;
-            this.client.on('qr', (qr) => this.on_qr_code(qr));
-            this.client.on('auth_failure', (msg) =>
+            this.client.on("disconnected", (reason) =>
+                this.state_changed(reason)
+            );
+            this.client.on("change_state", (state) =>
+                this.state_changed(state)
+            );
+            this.client.on("qr", (qr) => this.on_qr_code(qr));
+            this.client.on("auth_failure", (msg) =>
                 this.on_authenitcation_failed(msg)
             );
-            this.client.on('authenticated', (session) =>
+            this.client.on("authenticated", (session) =>
                 this.on_authenticated(session)
             );
-            this.client.on('ready', () => this.onReady());
-            this.client.on('message', (message) => this.on_message(message));
+            this.client.on("ready", () => this.onReady());
+            this.client.on("message", (message) => this.on_message(message));
         });
+    }
+
+    state_changed(state) {
+        switch (state) {
+            default:
+                this.connected = false;
+                log.warn("WhatsApp client state changed: " + state);
+                setTimeout(() => {
+                    if (!this.connected) {
+                        log.error("Lost connection to WhatsApp and no reconnection wihting five seconds happened. Notifying XMPP");
+                        this.bridge.get_xmpp().send_as_transfer("Bridge lost connection to WhatsApp");
+                    }
+                }, 5000)
+                break;
+            case "CONNECTED":
+                log.info("WhatsApp client connected"));
+                this.connected = true;
+                break;
+        }
     }
 
     on_message(message) {
@@ -49,30 +75,28 @@ class WAClient {
     }
 
     on_qr_code(qr) {
-        log.debug('Received QRCode: ' + qr);
-        QRCode.toString(qr, (err, str) =>
-            this.on_qr_code_encoded(err, str)
-        );
+        log.debug("Received QRCode: " + qr);
+        QRCode.toString(qr, (err, str) => this.on_qr_code_encoded(err, str));
     }
 
     on_qr_code_encoded(err, str) {
         if (err) {
-            log.error('Error while generating QR Code: ' + err);
+            log.error("Error while generating QR Code: " + err);
         } else {
             this.qr_code = str;
-            log.info('===========================================');
-            log.info('WhatsApp Web QR Code:');
+            log.info("===========================================");
+            log.info("WhatsApp Web QR Code:");
             log.info(str);
-            log.info('===========================================');
+            log.info("===========================================");
         }
     }
 
     on_authenticated(session) {
-        log.info('Client authenticated');
+        log.info("Client authenticated");
         this.session_cfg = session;
-        fs.open('./whats_app_client_session.json', 'w', (err, fd) => {
+        fs.open("./whats_app_client_session.json", "w", (err, fd) => {
             if (err) {
-                log.error('Error while saving WhatsApp session: ' + err);
+                log.error("Error while saving WhatsApp session: " + err);
                 return;
             }
             fs.writeSync(fd, JSON.stringify(this.session_cfg));
@@ -80,18 +104,20 @@ class WAClient {
     }
 
     on_authenitcation_failed(msg) {
-        log.err('Client authentication failed: ' + msg);
+        log.err("Client authentication failed: " + msg);
     }
 
     onReady() {
-        log.info('Client ready');
+        log.info("Client ready");
     }
 
     process_xmpp_message(message) {
-        this.client.getChatById(message.chat_id).then(chat => {
-            this.client.sendMessage(message.chat_id, message.data.children[0].children[0]);
+        this.client.getChatById(message.chat_id).then((chat) => {
+            this.client.sendMessage(
+                message.chat_id,
+                message.data.children[0].children[0]
+            );
         });
-        
     }
 }
 
